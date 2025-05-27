6b5fdd34bd20bd05aa92c64484fcd75fa94c335e
Darko Božić <darkojbozic@gmail.com>
2025-05-27 18:21:09 +0200

    Clean-up Loader component render

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import "./loader.css";
 const Loader = () => {
   return (
     <div className="car-loader-container">
-      <img className="car-loading" src="/images/logo.webp" alt="" />;
+      <img className="car-loading" src="/images/logo.webp" alt="" />
     </div>
   );
 };
